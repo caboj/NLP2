9a30ad83e08cfee7d1f9213d8a83154cfb53bccd
jacob5677688 <jacob.verdegaal@student.uva.nl>
2015-04-17 12:52:54 +0200

    not sure if likelihood is correct - removed indexing fault

--- project1/proj1.py
+++ project1/proj1.py
@@ -1,34 +1,34 @@
 import nltk
-from math import log
+from math import log, e, pow
 from multiprocessing import Pool
 
 class proj1(object):
 
     def __init__(self,model=2):
-        self.enC = self.loadData('corpus_1000.en')
-        self.nlC = self.loadData('corpus_1000.nl')
+        enC = self.loadData('corpus_1000.en')
+        nlC = self.loadData('corpus_1000.nl')
 
         # append 'NULL' to english sentences
         # and initialize t(f|e) 
         self.tfe = {}
         LMs = []
-        for i in range(len(self.enC)):
-            self.enC[i].append('NULL')
-            for f in self.nlC[i]:
-                l = len(self.enC[i])
-                m = len(self.nlC[i])
+        for k in range(len(enC)):
+            enC[k].append('NULL')
+            for f in nlC[k]:
+                l = len(enC[k])
+                m = len(nlC[k])
                 if (l,m) not in LMs:
                     LMs.append((l,m))
-                for e in self.enC[i]:
+                for e in enC[k]:
                     self.tfe[(f,e)] = 1
 
         # normalize tfe to uniform distribution
-        unifP = log( (float) (1)/len(self.tfe))
+        unifP = (float) (1)/len(self.tfe)
         for p in self.tfe:
             self.tfe[p] = unifP
 
         # set up the data structures 
-        cjilms_all = [[(j+1,i+1,l,m) for j in range(m) for i in range(l) ] for (l,m) in LMs]
+        cjilms_all = [[(j+1,i+1,l,m) for j in range(l) for i in range(m) ] for (l,m) in LMs]
         self.cjilm = {}
         self.cilm = {}
         for cjilms in cjilms_all:
@@ -39,13 +39,15 @@ class proj1(object):
         if model==1:
             self.qjilm = self.cjilm
         elif model == 2:
-            unifQ = 1/len(self.cjilm)
+            unifQ = (float) (1)/len(self.cjilm)
             self.qjilm = {q:unifQ for q in self.cjilm}
-        self.IBM(self.enC,self.nlC,1)
-        
+        ltqs = self.IBM(enC,nlC,1)
+
+        print(ltqs)
     # following Collinls lecture notes p.21 
     def IBM(self, enC, nlC,model,T=5):
 
+        ltqs = []
         it = 0
         while T > 0:
             print('iteration: ', it)
@@ -57,7 +59,7 @@ class proj1(object):
             self.cilm = {c:0 for c in self.cilm}
         
             print('maximizing...')
-            self.maximize((self.enC,self.nlC),1)
+            self.maximize((enC,nlC),1)
             # use 4 cpu's to count
             #with Pool(4) as p:
             #    p.map(self.maximize,[(enC[:250],nlC[:250]),(enC[251:500],nlC[251:500]),(enC[501:750],nlC[501:750]),(enC[751:1000],nlC[751:1000])])
@@ -66,9 +68,11 @@ class proj1(object):
             self.tfe = {(f,e):(self.cef[e,f]/self.ce[e]) for (f,e) in self.tfe}
             self.qjilm = {(j,i,l,m):(self.cjilm[(j,i,l,m)]/self.cilm[(i,l,m)]) for (j,i,l,m) in self.qjilm}
 
+            # determine likelihood
+            ltqs.append((T,self.ltq(enC,nlC)))
             T -= 1
 
-
+        return ltqs
     def maximize(self,sents,model):
         enC,nlC = sents
 
@@ -91,10 +95,10 @@ class proj1(object):
         for k in range(len(enC)):
             m = len(nlC[k])
             l = len(enC[k])
-            for j in range(m):
-                for i in range(l):
-                    e = enC[k][i]
-                    f = nlC[k][j]
+            for i in range(m):
+                for j in range(l):
+                    e = enC[k][j]
+                    f = nlC[k][i]
                     if model == 1:
                         delta = self.tfe[(f,e)] / tf[f]
                     elif model == 2:
@@ -104,11 +108,27 @@ class proj1(object):
                     self.cjilm[(j+1,i+1,l,m)] += delta
                     self.cilm[(i+1,l,m)] += delta
 
-        
+    
+    def ltq(self, enC, nlC):
+        pf = 0
+        for k in range(len(enC)):
+            pf_k = 0
+            l = len(enC[k])
+            m = len(nlC[k])
+            for i in range(len(nlC[k])):
+                pfa_k = 0
+                for j in range(len(enC[k])):                
+                    if ((j+1, i+1,l,m) in self.qjilm and (nlC[k][i],enC[k][j]) in self.tfe):
+                        pfa_k += self.qjilm[j+1,i+1,l,m]*self.tfe[(nlC[k][i],enC[k][j])]
+                pf_k += log(pfa_k)
+            pf+=pow(e,pf_k)
+        return pf
+            
+                
     
     def loadData(self, fileloc):
         toker = nltk.tokenize.RegexpTokenizer(r'((?<=[^\w\s])\w(?=[^\w\s])|(\W))+', gaps=True)
-        st = nltk.stem.porter.PorterStemmer()
+        #st = nltk.stem.porter.PorterStemmer()
         
         sents = []
         with open(fileloc) as f:
