5fd75afaa53ed8457b890bb140158577722c3168
jacob5677688 <jacob.verdegaal@student.uva.nl>
2015-04-19 08:16:04 +0200

    fixed delta for model 2

--- project1/proj1.py
+++ project1/proj1.py
@@ -7,7 +7,7 @@ class proj1(object):
     def __init__(self,model=2):
         enC = self.loadData('corpus_1000.en')
         nlC = self.loadData('corpus_1000.nl')
-
+        self.model = model
         # append 'NULL' to english sentences
         # and initialize t(f|e) 
         self.tfe = {}
@@ -36,20 +36,21 @@ class proj1(object):
                 self.cjilm[(j,i,l,m)] = 0
                 self.cilm[(i,l,m)] = 0
 
-        if model==1:
+        if self.model==1:
             self.qjilm = self.cjilm
-        elif model == 2:
+        elif self.model == 2:
             unifQ = (float) (1)/len(self.cjilm)
             self.qjilm = {q:unifQ for q in self.cjilm}
-        ltqs = self.IBM(enC,nlC,1)
+        ltqs = self.IBM(enC,nlC,model)
 
         print(ltqs)
     # following Collinls lecture notes p.21 
     def IBM(self, enC, nlC,model,T=5):
-
+        d=100
+        lastltq = 0 
         ltqs = []
         it = 0
-        while T > 0:
+        while d > 0.000000000001:
             print('iteration: ', it)
             it += 1
             
@@ -59,21 +60,27 @@ class proj1(object):
             self.cilm = {c:0 for c in self.cilm}
         
             print('maximizing...')
-            self.maximize((enC,nlC),1)
+            self.maximize((enC,nlC))
             # use 4 cpu's to count
             #with Pool(4) as p:
-            #    p.map(self.maximize,[(enC[:250],nlC[:250]),(enC[251:500],nlC[251:500]),(enC[501:750],nlC[501:750]),(enC[751:1000],nlC[751:1000])])
+            #    p.map(self.maximize,[(enC[:250],nlC[:250],1),(enC[251:500],nlC[251:500],1),(enC[501:750],nlC[501:750],1),(enC[751:1000],nlC[751:1000],1)])
         
             print('estimating...')
             self.tfe = {(f,e):(self.cef[e,f]/self.ce[e]) for (f,e) in self.tfe}
             self.qjilm = {(j,i,l,m):(self.cjilm[(j,i,l,m)]/self.cilm[(i,l,m)]) for (j,i,l,m) in self.qjilm}
-
+            
+            
             # determine likelihood
-            ltqs.append((T,self.ltq(enC,nlC)))
-            T -= 1
-
+            ltq = self.ltq(enC,nlC)
+            ltqs.append((ltq,it))
+            #d =  ltq - lastltq
+            print('-- d: ',d)
+            print('-- ltq: ', ltq)
+            lastltq = ltq
+    
         return ltqs
-    def maximize(self,sents,model):
+    
+    def maximize(self,sents):
         enC,nlC = sents
 
         # precompute denominators of delta
@@ -84,35 +91,38 @@ class proj1(object):
             elif f not in tf:
                 tf[f] = self.tfe[(f,e)]
 
-        qilm = {}
-        for (j,i,l,m) in self.qjilm:
-            if (i,l,m) in qilm:
-                qilm[(i,l,m)] += self.qjilm[(j,i,l,m)]
-            elif (i,l,m) not in qilm:
-                qilm[(i,l,m)] = self.qjilm[(j,i,l,m)]
+                
 
         # compute new counts to maximize estimation
         for k in range(len(enC)):
             m = len(nlC[k])
             l = len(enC[k])
             for i in range(m):
+                f = nlC[k][i]
+                if self.model == 2:
+                    denom = 0
+                    for J in range(l):
+                        denom += self.tfe[(f,enC[k][J])]*self.qjilm[(J+1,i+1,l,m)]
                 for j in range(l):
                     e = enC[k][j]
-                    f = nlC[k][i]
-                    if model == 1:
+                    if self.model == 1:
                         delta = self.tfe[(f,e)] / tf[f]
-                    elif model == 2:
-                        delta = self.tfe[(f,e)]*self.qjilm[(j+1,i+1,l,m)] / (tf[f]+qilm[i,l,m])
+                    elif self.model == 2:
+                        delta = self.tfe[(f,e)]*self.qjilm[(j+1,i+1,l,m)] / denom
                     self.cef[(e,f)] += delta
                     self.ce[e] += delta
                     self.cjilm[(j+1,i+1,l,m)] += delta
                     self.cilm[(i+1,l,m)] += delta
 
+
+    """
+    Determine LogLikelihood
+    """
     
     def ltq(self, enC, nlC):
         pf = 0
         for k in range(len(enC)):
-            pf_k = 0
+            pf_k = 0 
             l = len(enC[k])
             m = len(nlC[k])
             for i in range(len(nlC[k])):
@@ -123,7 +133,8 @@ class proj1(object):
                 pf_k += log(pfa_k)
             pf+=pow(e,pf_k)
         return pf
-            
+               
+    
                 
     
     def loadData(self, fileloc):
