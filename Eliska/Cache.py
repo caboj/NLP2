c87369399ffa394c280c01cf629424d6140624e3
kofolaloka <e.hartlova@gmail.com>
2015-04-19 13:10:08 +0200

    Eliska...again?

---
+++ Eliska/Cache.py
@@ -0,0 +1,43 @@
+import os
+
+class Cache(object):
+    def __init__(self, fileName, storageObject):
+        self.cache = storageObject
+        self.file = 'Cache/'+fileName
+        self.load()
+        self.saveCache = True
+
+    def load(self):
+        try:
+            print 'Reading cache %s...' % self.file
+            with open(self.file, 'r') as f:
+                cache = f.read()
+                try:
+                    self.cache = eval(cache)
+                except Exception, e:
+                    print e
+        except IOError:
+            pass
+
+    def replace(self, fileName, storageObject):
+        if self.saveCache:
+            oldFile = self.file
+            # save new cache
+            self.cache = storageObject
+            self.file = fileName
+            self.save()
+            if self.saveCache: # self.save() may have turned saving off
+                # delete old cache
+                try:
+                    os.remove(oldFile)
+                except OSError:
+                    pass
+
+    def save(self):
+        if self.saveCache:
+            try:
+                with open(self.file, 'w') as f:
+                    f.write(str(self.cache))
+            except IOError:
+                self.saveCache = False
+                print "Failed while writing cache. Cache will no longer be saved..."
